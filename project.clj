a712ea70486aaba7af6d45d26e282d7178310982
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-08-20 15:22:31 -0600

    Aot lamina and gloss

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.datomic/datomic-free "0.9.4815.12"]
                  [aleph "0.3.4-SNAPSHOT"]]
-  :aot [aleph.tcp]
+  :aot [aleph.tcp lamina.core gloss.core]
   :main retro.core)
